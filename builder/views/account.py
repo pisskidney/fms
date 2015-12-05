7569f9b08e4a7606b6c0b0d60910683b5bbf74ef
Peter Horvath <horvthpeter@gmail.com>
2015-12-05 05:51:33 +0000

    update2

--- builder/views/account.py
+++
@@ -1,8 +0,0 @@
-from django.http import HttpResponse
-from django.shortcuts import redirect, render
-from django.views.generic.base import View
-
-
-class AccountView(View):
-    def get(self, request):
-        return render(request, 'account.html')
